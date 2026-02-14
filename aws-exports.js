e7440e81cfd1bc25ec4107eca716541600df6630
Carlos Zamboni <carloszambrozio@gmail.com>
2026-02-14 01:44:48 -0300

    backend atualizado com federation

--- aws-exports.js
+++
@@ -1,9 +0,0 @@
-const awsExports = {
-  aws_project_region: 'us-east-1',
-  aws_appsync_graphqlEndpoint: process.env.NEXT_PUBLIC_APPSYNC_GRAPHQL_ENDPOINT,
-  aws_appsync_region: 'us-east-1',
-  aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: process.env.NEXT_PUBLIC_APPSYNC_API_KEY
-};
-
-export default awsExports;
--- chat-ui/basic.test.js
+++
@@ -1,6 +0,0 @@
-const test = require('node:test');
-const assert = require('node:assert/strict');
-
-test('dummy test passes', () => {
-  assert.equal(1 + 1, 2);
-});
